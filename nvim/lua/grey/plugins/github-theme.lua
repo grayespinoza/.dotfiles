f4b25509e26eaa5ae6b8db0d5b59819bf5a6f2e3
Grey <grey@grayespinoza.dev>
2025-02-13 15:53:11 -0800

    added wezterm github dark theme

--- nvim/lua/grey/plugins/github-theme.lua
+++ nvim/lua/grey/plugins/github-theme.lua
@@ -5,7 +5,9 @@ return {
   priority = 1000,
   config = function()
     require("github-theme").setup({
-      -- ...
+      options = {
+        transparent = true,
+      }
     })
 
     vim.cmd("colorscheme github_dark_high_contrast")
